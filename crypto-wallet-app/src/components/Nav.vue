f3994273a515640e54d7a3859618828022863334
Pierre Abeille <pierre.abeille@g-mail.fr>
2021-03-16 11:02:37 +0100

    [FEAT] Passage à Vue 3.x

---
+++ crypto-wallet-app/src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <li>Accueil</li>
+            <li>Acheter</li>
+            <li>Vendre</li>
+            <li>Gestion</li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+}
+console.log('ok')
+</script>
+
+<style>
+    ul {
+        display : inline;
+    }
+
+    .menu {
+        background-color: #aaa;
+    }
+</style>
