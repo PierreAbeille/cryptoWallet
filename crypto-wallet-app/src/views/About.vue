0f361964fabb9b611bcc2672d17669c92aafa9b3
Pierre Abeille <pierre.abeille@g-mail.fr>
2021-04-21 00:54:54 +0200

    [FEAT] Finished all charts <3

--- crypto-wallet-app/src/views/About.vue
+++ crypto-wallet-app/src/views/About.vue
@@ -2,11 +2,17 @@
   <div class="about">
     <h1>Qu'est-ce que CryptoWallet ?</h1>
     <div class="explications">
-      <p>CryptoWallet est un site de simulation d'achat et de revente de cryptomonnaie développé par Pierre Abeille et Yohann Markuc.
-        Le site est développé principalement avec VUEjs, Coingecko et Charjs (?).
+      <p>CryptoWallet est un site de simulation d'achat et de revente de cryptomonnaie développé par <a href="https://github.com/PierreAbeille">Pierre Abeille</a> et <a href="https://github.com/ymarkuc">Yohann Markuc</a>. <br>
+        Le site est développé principalement avec <a href="https://v3.vuejs.org/guide/introduction.html">Vue.js</a>, <a href="https://www.coingecko.com/api/documentations/v3">Coingecko API</a> et <a href="https://apexcharts.com/docs/installation/">ApexCharts</a>.
         On peut y consulter le cours actuel du marché, et acheter / vendre des crypto en fonction de notre portefeuille et de la valeur à l'instant T de celles ci.
+        On peut aussi y consulter la valorisation de nos portefeuilles au cours du temps.
       </p>
-      <img src="../assets/crypto.gif" alt="">
+      <img src="../assets/crypto.svg" alt="">
+			<img src="../assets/update.svg" alt="">
+			<p>
+				Actuellement le volet de gauche ne se met pas à jour en temps réel lors des transactions. Il faut actualiser la page pour que celui ci se mette à jour. <br>
+				Une future mise à jour devrait régler ce problème.
+			</p>
     </div>
   </div>
 </template>
@@ -14,7 +20,7 @@
 <script>
 
 export default {
-  name: 'About'
+	name: 'About'
 }
 </script>
 
@@ -23,16 +29,29 @@ export default {
     margin-top: 5vh;
     display: flex;
     justify-content: space-between;
+		flex-wrap: wrap;
   }
 
   p{
-    width: 40vw;
-    font-size: 1.8rem;
-    line-height: 1.8;
+    width: 35vw;
+    font-size: 1.6rem;
+    line-height: 1.7;
   }
 
   img {
     height: auto;
     width: 20vw;
+		margin-bottom: 10vh;
   }
+
+	a {
+		text-decoration: none;
+		color: #42b983;
+		font-weight: bold;
+	}
+
+	a:hover {
+		opacity : 0.75;
+		text-decoration: underline;
+		}
 </style>
--- crypto-wallet-app/src/views/Boursicoter.vue
+++ crypto-wallet-app/src/views/Boursicoter.vue
@@ -1,72 +1,79 @@
 <template>
-  <div class="boursicoter">
-    <h1>Evolution des cryptos par rapport au dollar</h1>
-    <div class="chart">
-    </div>
-    <!-- @sendForm est une classe personnalisée -->
-    <!-- Elle permet de passer en parametre du composant une fonction du parent-->
-    <!-- listeCrypto correspond à l'objet json crée par la reqûete effectué dans ce composant  -->
-    <!-- Ici aussi ça permet d'envoyer au composant enfant un objet -->
-    <BoursForm @sendForm="sendForm" :listeCrypto="rsp"/>
-  </div>
+	<div class="boursicoter">
+		<h1>Evolution des cryptos par rapport à l'euro</h1>
+		<div class="chart">
+			<BoursChart/>
+		</div>
+		<!-- @sendForm est une classe personnalisée -->
+		<!-- Elle permet de passer en parametre du composant une fonction du parent-->
+		<!-- listeCrypto correspond à l'objet json crée par la reqûete effectué dans ce composant  -->
+		<!-- Ici aussi ça permet d'envoyer au composant enfant un objet -->
+		<BoursForm @sendForm="sendForm" :listeCrypto="rsp"/>
+	</div>
 </template>
 
 <script>
 
 import BoursForm from '@/components/BoursForm.vue'
+import BoursChart from '@/components/BoursChart.vue'
 // import de axios en tant que module
 import axios from 'axios'
 // Ici on définie l'URL de base de l'api, celle des requêtes étant spécifiée à part
 axios.defaults.baseURL = 'http://localhost:3000'
 
 export default {
-  name: 'Boursicoter',
+	name: 'Boursicoter',
 
-  components: {
-    BoursForm
-  },
+	components: {
+		BoursForm,
+		BoursChart
+	},
 
-  data: () => ({
-    error: '',
-    rsp: [],
-    dataReady: false
-  }),
+	data: () => ({
+		error: '',
+		rsp: [],
+		dataReady: false
+	}),
 
-  // En utilisant axios l'appel de l'api est plus rapide et lisible
-  // Utilisation de async obligatoire avec await. Il s'agit d'une fonction asynchrone
-  async mounted () {
-    // L'URL de la requête est à part, c'est plus lisible
-    // L'utilisation de await permet d'éviter les '.then' à répétition
-    const response = await axios.get('/allwallets')
-    // "Tu lances cette opération qu'une fois la bonne réception du résultat de la requête"
-    this.rsp = response.data
-  },
-  methods: {
-    // Fonction asynchrone
-    // Elle prend en parametre un form
-    // Elle permet d'appeler une fonction d'un composant enfant. On réalise une requête à partir du parent
-    async sendForm (form) {
-      // 'achat' est un objet de form dans le composant cible (@/components/BoursForm.vue)
-      if (form.achat) {
-        // requete pour achat
-        const response = await axios.post('/deposit', form)
-        console.log(response)
-        console.log('acheter')
-        console.log(form)
-      } else {
-        // requete pour vendre
-        const response = await axios.post('/withdraw', form)
-        console.log(response)
-        console.log('vendre')
-        console.log(form)
-      }
-    }
-  }
+	// En utilisant axios l'appel de l'api est plus rapide et lisible
+	// Utilisation de async obligatoire avec await. Il s'agit d'une fonction asynchrone
+	async mounted () {
+		// L'URL de la requête est à part, c'est plus lisible
+		// L'utilisation de await permet d'éviter les '.then' à répétition
+		const response = await axios.get('/allwallets')
+		// "Tu lances cette opération qu'une fois la bonne réception du résultat de la requête"
+		this.rsp = response.data
+	},
+	methods: {
+		// Fonction asynchrone
+		// Elle prend en parametre un form
+		// Elle permet d'appeler une fonction d'un composant enfant. On réalise une requête à partir du parent
+		async sendForm (form) {
+			// 'achat' est un objet de form dans le composant cible (@/components/BoursForm.vue)
+			if (form.achat) {
+				// requete pour achat
+				const response = await axios.post('/deposit', form)
+				console.log(response)
+				console.log('acheter')
+				console.log(form)
+			} else {
+				// requete pour vendre
+				const response = await axios.post('/withdraw', form)
+				console.log(response)
+				console.log('vendre')
+				console.log(form)
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
-  h1 {
-    margin-bottom: 5vh;
-  }
+	h1 {
+		margin-bottom: 5vh;
+	}
+
+	.chart {
+		margin-bottom: 12vh;
+	}
 </style>
